229e6f0d2f26baee7786c2516fd42b4de3565184
eranabir <eran@fireblocks.com>
2021-08-12 01:18:13 +0300

    fixing adding item to pending ordr in firebase

--- src/store/actions/cartActions.js
+++ src/store/actions/cartActions.js
@@ -4,13 +4,15 @@ export const addToCart = (phone) => {
     return (dispatch, getState) => {
         delete phone.id
         const ordersReducer = getState().ordersReducer
+        const cartReducer = getState().cartReducer
         const pendingOrderId = ordersReducer.pendingOrder.id
+        const cartItems = cartReducer.cartItems
         const pendingOrderItems = ordersReducer.pendingOrder.items
         const user = currentUser()
 
         if (pendingOrderId) {
             firestore.collection('users').doc(user.uid).collection('orders').doc(pendingOrderId).update({
-                items:[...pendingOrderItems,phone]
+                items:[...cartItems,...pendingOrderItems,phone]
             }).then(() => {
                 dispatch({
                     type: 'ADD_ITEM_TO_CART',
